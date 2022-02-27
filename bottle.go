cf04f74f7ef2d912f18f5c3b068dadaed9d8937b
auual <ding@ibyte.me>
2022-02-27 12:42:40 +0800

    push: dev branch.

--- bottle.go
+++ bottle.go
@@ -9,8 +9,10 @@ import (
 	"fmt"
 	"os"
 	"sync"
+	"time"
 )
 
+// Global universal block
 var (
 
 	// Data storage directory
@@ -26,10 +28,10 @@ var (
 	index map[uint64]*record
 
 	// Current data file meter
-	dataFileCount int32 = 0
+	dataFileIdentifier int64 = time.Now().Unix()
 
 	// Old data file mapping
-	fileList map[int32]*os.File
+	fileList map[int64]*os.File
 
 	// Data file name extension
 	dataFileSuffix = ".data"
@@ -43,9 +45,22 @@ var (
 	// Perm Default file operation permission
 	Perm = os.FileMode(0755)
 
-	// defaultMaxFileSize file size
+	// Default max file size
 	defaultMaxFileSize int64 = 2 << 8 << 20 // 2 << 8 = 512 << 20 = 536870912 kb
 
+	// Default garbage collection merge threshold value
+	defaultMergeThresholdValue = 1024
+
+	// Index delete key count
+	deleteKeyCount = 0
+)
+
+// Higher-order function blocks
+var (
+	// Opens a file by specifying a mode
+	openDataFile = func(flag int) (*os.File, error) {
+		return os.OpenFile(fmt.Sprintf("%s%d%s", dataRoot, dataFileIdentifier, dataFileSuffix), flag, Perm)
+	}
 )
 
 // record Mapping Data Record
@@ -96,17 +111,17 @@ func createActiveFile() error {
 func buildDataFile() (*os.File, error) {
 	mutex.Lock()
 	defer mutex.Unlock()
-	dataFileCount += 1
-	return os.OpenFile(fmt.Sprintf("%s%d%s", dataRoot, dataFileCount, dataFileSuffix), FRW, Perm)
+	dataFileIdentifier = time.Now().Unix()
+	return openDataFile(FRW)
 }
 
 // File archiving is triggered when the data file is full
 func exchangeFile() error {
 	mutex.Lock()
 	defer mutex.Unlock()
-	active.Close()
-	if file, err := os.OpenFile(fmt.Sprintf("%s%d%s", dataRoot, dataFileCount, dataFileSuffix), FR, Perm); err == nil {
-		fileList[dataFileCount] = file
+	_ = active.Close()
+	if file, err := openDataFile(FR); err == nil {
+		fileList[dataFileIdentifier] = file
 	}
 	return createActiveFile()
 }
