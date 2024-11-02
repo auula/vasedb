d4b9e2eb2b74dd8ac380045d9b65cf65325fc1a6
Leon Ding <deen.job@qq.com>
2024-11-02 20:35:10 +0800

    fix: gossip protocol server.
    fix: gossip protocol server.

--- server/gossip/gossip.go
+++ server/gossip/gossip.go
@@ -10,35 +10,36 @@ import (
 
 const minNeighbors = 3 // 最小节点数，奇数
 
-// 类似于私有化 Node {} 构造器
-// 只能通过 NewNode 进行实例化一个节点
-type innerNode struct {
-	ID        string // 节点唯一标识
-	Address   string // 节点地址
-	Heartbeat int    // 心跳计数
-	Timestamp int64  // 最新心跳时间
-	Alive     bool   // 节点状态
-	Hash      uint32 // 节点哈希值，用于一致性哈希
-}
+type (
+	// 类似于私有化 Node {} 构造器
+	// 只能通过 NewNode 进行实例化一个节点
+	innerNode struct {
+		ID        string // 节点唯一标识
+		Address   string // 节点地址
+		Heartbeat int    // 心跳计数
+		Timestamp int64  // 最新心跳时间
+		Alive     bool   // 节点状态
+		Hash      uint32 // 节点哈希值，用于一致性哈希
+	}
+	innerCluster struct {
+		Neighbors map[string]*Node // 所有已知节点的信息
+		Self      *Node            // 本节点信息
+		Mutex     sync.Mutex       // 保护 Nodes 的并发访问
+		Interval  time.Duration    // 心跳间隔
+		Timeout   time.Duration    // 失效阈值
+		HashRing  []*Node          // 一致性哈希存储范围的键
+	}
 
-type innerCluster struct {
-	Neighbors map[string]*Node // 所有已知节点的信息
-	Self      *Node            // 本节点信息
-	Mutex     sync.Mutex       // 保护 Nodes 的并发访问
-	Interval  time.Duration    // 心跳间隔
-	Timeout   time.Duration    // 失效阈值
-	HashRing  []*Node          // 一致性哈希存储范围的键
-}
+	// Node 协议集群中的节点
+	Node struct {
+		innerNode
+	}
 
-// Node 协议集群中的节点
-type Node struct {
-	innerNode
-}
-
-// Cluster 协议的集群集合
-type Cluster struct {
-	innerCluster
-}
+	// Cluster 协议的集群集合
+	Cluster struct {
+		innerCluster
+	}
+)
 
 func NewNode(id, addr string) *Node {
 	node := &Node{
@@ -88,9 +89,13 @@ func (c *Cluster) AddNodes(nodes ...Node) error {
 
 	// 设置节点哈希值并将节点加入到邻居中
 	for _, node := range nodes {
-		c.Neighbors[node.ID] = &node
-		// 节点对应哈希值也要放到一致性节点环中
-		c.HashRing = append(c.HashRing, &node)
+		_, existing := c.Neighbors[node.ID]
+		// 不存在节点就放入到集群中
+		if !existing {
+			c.Neighbors[node.ID] = &node
+			// 节点对应哈希值也要放到一致性节点环中
+			c.HashRing = append(c.HashRing, &node)
+		}
 	}
 
 	// 对一致性哈希环中的节点按哈希值排序
