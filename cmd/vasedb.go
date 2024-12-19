8ed5509eba07ee14855cb173876270525285826b
Leon Ding <deen.job@qq.com>
2024-12-19 20:11:27 +0800

    fix: log-structured file system.

--- cmd/vasedb.go
+++ cmd/vasedb.go
@@ -20,6 +20,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 	"time"
 
 	"github.com/auula/vasedb/clog"
@@ -124,12 +125,11 @@ type flags struct {
 }
 
 // runAsDaemon 以守护进程模式运行
+// 后台守护进程模式启动，创建一个与当前程序相同的命令
 func runAsDaemon() {
-	// 后台守护进程模式启动，创建一个与当前程序相同的命令
-	cmd := exec.Command(os.Args[0], utils.SplitArgs(utils.TrimDaemon(os.Args))...)
-	// 如果需要传递环境变量信息
+
+	cmd := exec.Command(os.Args[0], splitArgs(trimDaemon(os.Args))...)
 	cmd.Env = os.Environ()
-	// 从当前进程启动守护进程
 	err := cmd.Start()
 	if err != nil {
 		clog.Failed(err)
@@ -181,4 +181,40 @@ func parseFlags() (fl *flags) {
 	flag.BoolVar(&daemon, "daemon", false, "--daemon whether to run with a daemon.")
 	flag.Parse()
 	return
+}
+
+// TrimDaemon 从 os.Args 中移除 "-daemon" 参数
+func trimDaemon(args []string) []string {
+	var newArgs []string
+
+	// 遍历 args 切片
+	for i := 1; i < len(args); i++ {
+		// 当发现参数是符合标准时跳过当前参数
+		if args[i] == "-daemon" || args[i] == "--daemon" {
+			continue
+		}
+		newArgs = append(newArgs, args[i])
+	}
+
+	return newArgs
+}
+
+// SplitArgs 处理命令行参数以 = 分割
+func splitArgs(args []string) []string {
+	var newArgs []string
+
+	for i := 1; i < len(args); i++ {
+		// 分割 args 中的元素，否则命令行解析错误
+		if strings.Contains(args[i], "=") && strings.Count(args[i], "=") == 1 {
+			newArgs = append(newArgs, strings.Split(args[i], "=")...)
+		} else {
+			// 过滤掉 == 不合法过滤掉
+			if strings.Count(args[i], "=") > 1 {
+				continue
+			}
+			newArgs = append(newArgs, strings.Split(args[i], "=")...)
+		}
+	}
+
+	return newArgs
 }
